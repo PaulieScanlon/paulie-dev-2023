ffcf4f9eb200dfe9c2ba50fce13ed4b1a72986a2
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-05 07:57:12 -0400

    refactor: set prerender and change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel({
     edgeMiddleware: true,
   }),
