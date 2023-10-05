f1c0b1bc23992f39aa1844b6f9b5ab17f8560237
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-04 23:39:23 -0400

    refactor: change to hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel({
     edgeMiddleware: true,
   }),
