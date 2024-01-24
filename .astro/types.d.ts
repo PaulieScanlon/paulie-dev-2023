74783f49f0e29febc7f6d6cf7870ceb878e6facc
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-24 13:57:53 -0600

    docs: add qwik break from react article

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -585,6 +585,13 @@ declare module 'astro:content' {
   collection: "articles";
   data: InferEntrySchema<"articles">
 } & { render(): Render[".mdx"] };
+"2024/01/take-a-qwik-break-from-react-with-astro.mdx": {
+	id: "2024/01/take-a-qwik-break-from-react-with-astro.mdx";
+  slug: "2024/01/take-a-qwik-break-from-react-with-astro";
+  body: string;
+  collection: "articles";
+  data: InferEntrySchema<"articles">
+} & { render(): Render[".mdx"] };
 };
 "demos": {
 "2021/06/paulie-api.mdx": {
