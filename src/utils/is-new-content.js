5538f48de2588fcb2d940645a441117a251b902f
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-29 20:59:18 -0800

    feat: add new label to content cards

---
+++ src/utils/is-new-content.js
@@ -0,0 +1,10 @@
+export const isNewContent = (inputDate) => {
+  const currentDate = new Date();
+  const inputDateObj = new Date(inputDate);
+
+  const timeDifference = currentDate - inputDateObj;
+
+  const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+  return daysDifference < 30;
+};
