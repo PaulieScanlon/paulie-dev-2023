70a51d3b7b8a945e024385a41555364b93fc39a3
Paul Scanlon <pauliescanlon@gmail.com>
2024-04-14 12:07:13 -0500

    refactor: change groupBySlug to remove root pages

--- src/utils/group-by-slug.js
+++ src/utils/group-by-slug.js
@@ -1,17 +1,25 @@
 const groupBySlug = (array) => {
   const groupedData = array.reduce((acc, item) => {
     const { slug } = item;
-    const key = slug;
 
-    if (!acc[key]) {
-      acc[key] = {
-        slug: slug,
-        total: 0,
-      };
+    const parts = slug.split('/').filter((item) => item !== '');
+
+    if (parts.length > 2) {
+      // only apply a title if it's not E.g /posts/ or /articles/
+
+      const title = parts.slice(-1)[0].split('-').join(' ');
+      const key = slug;
+
+      if (!acc[key]) {
+        acc[key] = {
+          title: title,
+          slug: slug,
+          total: 0,
+        };
+      }
+
+      acc[key].total++;
     }
-
-    acc[key].total++;
-
     return acc;
   }, {});
 
