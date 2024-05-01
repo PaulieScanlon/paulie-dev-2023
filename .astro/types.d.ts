8e8546c1bdb85b529f91ed3fa32bf7ce6406d58a
Paul Scanlon <pauliescanlon@gmail.com>
2024-05-01 09:29:22 -0500

    docs: add typescript postgres article

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -626,6 +626,13 @@ declare module 'astro:content' {
   collection: "articles";
   data: InferEntrySchema<"articles">
 } & { render(): Render[".mdx"] };
+"2024/05/automatically-generate-types-for-your-postgresql-database.mdx": {
+	id: "2024/05/automatically-generate-types-for-your-postgresql-database.mdx";
+  slug: "2024/05/automatically-generate-types-for-your-postgresql-database";
+  body: string;
+  collection: "articles";
+  data: InferEntrySchema<"articles">
+} & { render(): Render[".mdx"] };
 };
 "demos": {
 "2021/06/paulie-api.mdx": {
