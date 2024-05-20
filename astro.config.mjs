b3d4fe71b5746c0d7f859170d15d61faf28cf38d
Paul Scanlon <pauliescanlon@gmail.com>
2024-05-19 21:52:02 -0500

    feat: add partytown to ga scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,10 @@ import qwikdev from '@qwikdev/astro';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
-
+import partytown from '@astrojs/partytown';
 const isProd = import.meta.env.PROD;
 
+// https://astro.build/config
 export default defineConfig({
   site: isProd ? 'https://paulie.dev' : 'http://localhost:4321',
   output: 'server',
@@ -51,15 +52,28 @@ export default defineConfig({
       ],
     }),
     mdx({
-      // optimize: {
-      //   customComponentNames: ['StackBlitz'],
-      // },
       syntaxHighlight: 'prism',
       rehypePlugins: [
         rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-        [rehypeExternalLinks, { rel: ['nofollow'], target: ['_blank'] }],
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: 'wrap',
+          },
+        ],
+        [
+          rehypeExternalLinks,
+          {
+            rel: ['nofollow'],
+            target: ['_blank'],
+          },
+        ],
       ],
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
     }),
   ],
   vite: {
