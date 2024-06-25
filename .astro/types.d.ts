8242875c2b54033a0269c1fa95cd6dca9ea76a18
Paul Scanlon <pauliescanlon@gmail.com>
2024-06-25 13:36:09 +0100

    docs: add google application credentials post

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1311,6 +1311,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024/06/how-to-use-google-application-json-credentials-in-environment-variables.mdx": {
+	id: "2024/06/how-to-use-google-application-json-credentials-in-environment-variables.mdx";
+  slug: "2024/06/how-to-use-google-application-json-credentials-in-environment-variables";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 "streams": {
 "2021/04/gatsby-deep-dives-with-queen-raae.mdx": {
