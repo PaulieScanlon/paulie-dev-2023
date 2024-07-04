937937ebc52f7470d7c6690f77c4ab0df7e1a8cd
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-04 18:43:30 +0100

    docs: add ssl certificates post

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1325,6 +1325,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024/07/how-to-use-postgresql-ssl-certificates-in-github-actions.mdx": {
+	id: "2024/07/how-to-use-postgresql-ssl-certificates-in-github-actions.mdx";
+  slug: "2024/07/how-to-use-postgresql-ssl-certificates-in-github-actions";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 "streams": {
 "2021/04/gatsby-deep-dives-with-queen-raae.mdx": {
