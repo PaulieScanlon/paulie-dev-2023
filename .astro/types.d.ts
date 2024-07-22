4a02a15b6b04a0bd40f851e935ddfdcab1230a62
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-22 19:30:28 +0100

    docs: add luzmo clerk dashboard embedding article

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -703,6 +703,13 @@ declare module 'astro:content' {
   collection: "articles";
   data: InferEntrySchema<"articles">
 } & { render(): Render[".mdx"] };
+"2024/07/nextjs-embedded-dashboard.mdx": {
+	id: "2024/07/nextjs-embedded-dashboard.mdx";
+  slug: "2024/07/nextjs-embedded-dashboard";
+  body: string;
+  collection: "articles";
+  data: InferEntrySchema<"articles">
+} & { render(): Render[".mdx"] };
 "2024/07/real-time-monitoring.mdx": {
 	id: "2024/07/real-time-monitoring.mdx";
   slug: "2024/07/real-time-monitoring";
