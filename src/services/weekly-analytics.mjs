0e08fa5e6c44d172af9f99e7f317ef0fd644e8e8
Paul Scanlon <pauliescanlon@gmail.com>
2024-08-11 11:32:39 +0100

    feat: add section list

--- src/services/weekly-analytics.mjs
+++ src/services/weekly-analytics.mjs
@@ -3,6 +3,39 @@ dotenv.config();
 import { BetaAnalyticsDataClient } from '@google-analytics/data';
 
 const init = async () => {
+  const reportLimit = 10;
+  const HIGHER = '🟢';
+  const LOWER = '🔻';
+  const SAME = '▫️';
+  const NEW = '⭐';
+
+  // const HIGHER = '↑';
+  // const LOWER = '↓';
+  // const SAME = '-';
+  // const NEW = '☆';
+
+  const formatReport = (rows) => {
+    return rows.map((row) => {
+      const { dimensionValues, metricValues } = row;
+
+      return {
+        url: `https://${dimensionValues[0].value}`,
+        title: dimensionValues[1].value.split('|')[1].trim(),
+        // title: dimensionValues[1].value,
+        count: metricValues[0].value,
+      };
+    });
+  };
+
+  const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      weekday: 'long',
+      year: 'numeric',
+    });
+  };
+
   try {
     // Get data from Google Analytics
     const credentials = JSON.parse(
@@ -13,7 +46,7 @@ const init = async () => {
       credentials,
     });
 
-    const [response] = await analyticsDataClient.runReport({
+    const [thisWeek] = await analyticsDataClient.runReport({
       property: `properties/${process.env.GA4_PROPERTY_ID}`,
       dateRanges: [
         {
@@ -31,24 +64,140 @@ const init = async () => {
       ],
       metrics: [
         {
-          name: 'activeUsers',
+          name: 'totalUsers',
         },
       ],
-      limit: 10,
+      limit: reportLimit,
       metricAggregations: ['MAXIMUM'],
     });
 
-    // Create the report string
-    const report = response.rows
-      .map((row, index) => {
-        const { dimensionValues, metricValues } = row;
+    const [lastWeek] = await analyticsDataClient.runReport({
+      property: `properties/${process.env.GA4_PROPERTY_ID}`,
+      dateRanges: [
+        {
+          startDate: '14daysAgo',
+          endDate: '7daysAgo',
+        },
+      ],
+      dimensions: [
+        {
+          name: 'fullPageUrl',
+        },
+        {
+          name: 'pageTitle',
+        },
+      ],
+      metrics: [
+        {
+          name: 'totalUsers',
+        },
+      ],
+      limit: reportLimit,
+      metricAggregations: ['MAXIMUM'],
+    });
 
-        return `${index + 1}. <https://${dimensionValues[0].value}|${dimensionValues[1].value}> | *x${
-          metricValues[0].value
-        }*`;
+    // Calculate total count
+    const calculateTotalCount = (results) => {
+      return results.reduce((total, { count }) => total + parseInt(count, 10), 0);
+    };
+
+    // Format the results
+    const thisWeekResults = formatReport(thisWeek.rows);
+    const lastWeekResults = formatReport(lastWeek.rows);
+
+    // console.log('thisWeek: ', JSON.stringify(thisWeekResults, null, 2));
+    // console.log('-----------------------------------------------------------------');
+    // console.log('lastWeek: ', JSON.stringify(lastWeekResults, null, 2));
+
+    const lastWeekMap = lastWeekResults.reduce((map, { url, count }) => {
+      map[url] = count;
+      return map;
+    }, {});
+
+    // Function to determine the status
+    const determineStatus = (count, lastWeekCount) => {
+      if (lastWeekCount === undefined) {
+        return NEW;
+      }
+      if (lastWeekCount === '0') {
+        return NEW;
+      }
+      const thisCount = parseInt(count, 10);
+      const previousCount = parseInt(lastWeekCount, 10);
+      if (thisCount > previousCount) {
+        return HIGHER;
+      } else if (thisCount < previousCount) {
+        return LOWER;
+      } else {
+        return SAME;
+      }
+    };
+
+    // Generate the report for this week
+    const report = thisWeekResults.map(({ url, title, count }, index) => {
+      const lastWeekCount = lastWeekMap[url]; // No need for '|| '0' since we handle 'undefined' in the status function
+      const status = determineStatus(count, lastWeekCount);
+
+      return {
+        position: index + 1, // Array index as position value (1-based index)
+        url,
+        title,
+        count: { thisWeek: count, lastWeek: lastWeekCount || '0' }, // Ensure lastWeekCount is displayed as '0' if not found
+        status,
+      };
+    });
+
+    // console.log('-----------------------------------------------------------------');
+    // console.log(report);
+
+    // Create the blocks
+    const slackBlocks = report
+      .flatMap((item, index) => {
+        const {
+          position,
+          url,
+          title,
+          count: { thisWeek, lastWeek },
+          status,
+        } = item;
+
+        return [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*${position}. <${url}|${title}>*\n\n${status} *This week: \`${`x${thisWeek}`}\`* / Last week: \`${`x${lastWeek}`}\``,
+            },
+          },
+          {
+            type: 'divider',
+          },
+        ];
       })
-      .join('\\n')
-      .replace(/\\n/g, '\n');
+      .slice(0, -1);
+
+    const blocks = [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: `📅 ${formatDate(new Date())}`,
+          emoji: true,
+        },
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'Top Ten Page Views for <https://www.paulie.dev|paulie.dev>',
+        },
+      },
+    ];
+
+    blocks.push(...slackBlocks);
 
     // Post message to Slack
     fetch(process.env.SLACK_WEBHOOK_URL, {
@@ -56,37 +205,7 @@ const init = async () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        blocks: [
-          {
-            type: 'header',
-            text: {
-              type: 'plain_text',
-              text: '📊 7 Days Google Analytics Report',
-              emoji: true,
-            },
-          },
-          {
-            type: 'divider',
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'plain_text',
-              text: 'Top ten page views.',
-            },
-          },
-          {
-            type: 'context',
-            elements: [
-              {
-                type: 'mrkdwn',
-                text: report,
-              },
-            ],
-          },
-        ],
-      }),
+      body: JSON.stringify({ blocks }),
     });
   } catch (error) {
     console.error(error);
