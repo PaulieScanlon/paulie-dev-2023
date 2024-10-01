7a5e862e65b87202ed6cfb6263a92604dd26aede
Paul Scanlon <pauliescanlon@gmail.com>
2024-10-01 10:18:10 +0700

    docs: add dev for rds demo

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -819,6 +819,13 @@ declare module 'astro:content' {
   collection: "demos";
   data: InferEntrySchema<"demos">
 } & { render(): Render[".mdx"] };
+"2024/09/dev-for-rds.mdx": {
+	id: "2024/09/dev-for-rds.mdx";
+  slug: "2024/09/dev-for-rds";
+  body: string;
+  collection: "demos";
+  data: InferEntrySchema<"demos">
+} & { render(): Render[".mdx"] };
 };
 "ghosts": {
 "2024/03/enterprise-design-systems.mdx": {
