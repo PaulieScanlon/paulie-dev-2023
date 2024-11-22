79326e373eeef816bb834019947f822e4f698d60
Paul Scanlon <pauliescanlon@gmail.com>
2024-11-22 09:46:53 +0700

    docs: add database per tenant article

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -761,6 +761,13 @@ declare module 'astro:content' {
   collection: "articles";
   data: InferEntrySchema<"articles">
 } & { render(): Render[".mdx"] };
+"2024/11/migrating-schemas.mdx": {
+	id: "2024/11/migrating-schemas.mdx";
+  slug: "2024/11/migrating-schemas";
+  body: string;
+  collection: "articles";
+  data: InferEntrySchema<"articles">
+} & { render(): Render[".mdx"] };
 };
 "demos": {
 "2021/06/paulie-api.mdx": {
