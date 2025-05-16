5678b1c7227d885a32e88d4842771c8515b8be74
Paul Scanlon <pauliescanlon@gmail.com>
2025-05-16 17:40:53 +0100

    fix: handle Neon errors better

--- src/components/latest-reactions-by-location.tsx
+++ src/components/latest-reactions-by-location.tsx
@@ -1,5 +1,4 @@
 import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
-import { formatNumber } from '../utils/format-number';
 
 import Loading from './loading';
 import emojis from '../utils/emojis';
@@ -24,18 +23,70 @@ const HappyAllCities = component$<Props>(({ period }) => {
       }
 
       const result = await response.json();
-
       data.value = result.data;
     } catch (error) {
+      data.value = [];
       console.error(error);
     }
   });
+
+  console.log(data.value);
 
   return (
     <div class='m-0 p-0 border rounded border-brand-outline bg-brand-surface'>
       <div class='p-4'>
         <div class='rounded border border-brand-outline p-4 bg-brand-background h-[415px] overflow-y-hidden'>
           {data.value ? (
+            <div class='overflow-y-auto overflow-x-auto shadow-inner h-full'>
+              <table class='m-0 table-auto text-base'>
+                <thead class='font-medium border-b-2 border-b-brand-outline'>
+                  <tr>
+                    <th class='p-4'>Reaction</th>
+                    <th class='p-4'>Date</th>
+                    <th class='p-4'>Country</th>
+                    <th class='p-4'>City</th>
+                    <th class='p-4'>Title</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.value.map((row, index) => {
+                    const { date, reaction, city, country, flag, title, slug } = row;
+
+                    const svg = emojis.find((emoji) => emoji.name === reaction);
+
+                    return (
+                      <tr key={index} class='border-b-0 even:bg-brand-surface odd:bg-brand-background'>
+                        <td class='align-middle flex p-4 justify-center whitespace-nowrap'>
+                          <svg
+                            role='img'
+                            aria-label='reaction-happy'
+                            xmlns='http://www.w3.org/2000/svg'
+                            class={`not-prose mt-2 inline-block rounded-full w-5 h-5 fill-brand-${svg.color}`}
+                            viewBox='0 0 32 32'
+                            fill='currentColor'
+                          >
+                            <path d={svg.d} />
+                          </svg>
+                        </td>
+                        <td class='align-middle p-4'>{formatDateNumber(date)}</td>
+                        <td class='align-middle flex items-center gap-2 p-4'>
+                          <span class='mt-1'>{flag}</span>
+                          {country}
+                        </td>
+                        <td class='align-middle p-4 whitespace-nowrap'>{city}</td>
+                        <td class='align-middle whitespace-nowrap p-4'>
+                          <a href={slug} class='text-sm'>
+                            {title}
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          ) : null}
+          {/* {data.value ? (
             <>
               {data.value.length ? (
                 <div class='overflow-y-auto overflow-x-auto shadow-inner h-full'>
@@ -88,7 +139,7 @@ const HappyAllCities = component$<Props>(({ period }) => {
                 </div>
               ) : (
                 <div class='flex items-center justify-center h-full'>
-                  <strong>No happy cities to display</strong>
+                  <strong>No data to display</strong>
                 </div>
               )}
             </>
@@ -96,7 +147,7 @@ const HappyAllCities = component$<Props>(({ period }) => {
             <div class='flex items-center justify-center h-full'>
               <Loading />
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </div>
